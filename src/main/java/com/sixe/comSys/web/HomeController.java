52faf9c8b1c9b48637e9cf71c46c701c15338981
liu <767035003@qq.com>
2017-02-24 20:32:00 +0800

    新建dtu项目

---
+++ src/main/java/com/sixe/comSys/web/HomeController.java
@@ -0,0 +1,84 @@
+package com.sixe.comSys.web;
+
+import com.sixe.comSys.base.imgCode.ValidateCode;
+import com.sixe.comSys.utils.HttpTools;
+import com.sixe.comSys.utils.Tools;
+import com.sun.istack.internal.logging.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+
+
+/**
+ * Created by labiStronger on 2017/2/24.
+ */
+@Controller
+@RequestMapping(value = "/home")
+public class HomeController {
+
+    private static final Logger logger = Logger.getLogger(HomeController.class);
+
+    @RequestMapping(value="login")
+    public String login(){
+        logger.info("进入登录页...");
+        System.out.println("进入登录页...");
+        return "/login";
+    }
+
+    /**
+     * 图片验证码
+     * @param request
+     * @param response
+     * @throws IOException
+     */
+    @RequestMapping("/randomImg.do")
+    public void randomImg(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // 设置响应的类型格式为图片格式
+        response.setContentType("image/jpeg");
+        //禁止图像缓存。
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setDateHeader("Expires", 0);
+        ValidateCode vCode = new ValidateCode(120,40,5,100,response);
+        vCode.write(response.getOutputStream());
+        request.getSession().setAttribute("code", vCode.getCode());
+    }
+
+    /**
+     * 验证图片验证码
+     * @param code
+     * @param request
+     * @param response
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value="/checkCode.do")
+    public String checkCode(String code,HttpServletRequest request, HttpServletResponse response){
+        String sessionCode = (String) request.getSession().getAttribute("code");
+		/*设置不区分大小写  全部转为大写比较*/
+        String newSessionCode = sessionCode.toUpperCase();
+        String newCode = code.toUpperCase();
+        if(newSessionCode.equals(newCode)){
+            return Tools.sendJson("SUC");
+        }
+        return Tools.sendJson("FIL");
+    }
+
+    @ResponseBody
+    @RequestMapping(value="/doLogin")
+    public String doLogin(String user_id,String pwd){
+
+        logger.info("userId:"+user_id+"\npwd:"+pwd);
+
+        String result = HttpTools.sendPost("http://139.129.239.172:7710/php/check_usr2.php","user_id="+user_id+"&pwd="+pwd);
+        System.out.println(result);
+        return "SUC";
+    }
+
+}
---
+++ src/main/webapp/common/myjs/login.js
@@ -0,0 +1,86 @@
+function login(){
+    var userName = $('#userName').val();
+    var password = $('#password').val();
+    var code = $('#code').val();
+    $("#errMsg").empty();
+    $("#errMsg").attr("class","msg");
+    if(userName.length == 0 || userName == ''){
+        $("#errMsg").html("请输入用户名");
+        return false;
+    }
+    if(password.length == 0 || password == '') {
+        $("#errMsg").html("请输入密码");
+        return false;
+    }
+    if(code.length == 0 || code == '') {
+        $("#errMsg").html("请输入验证码");
+        return false;
+    }
+    $("#errMsg").empty();
+    var code = $('#code').val();
+    $.ajax({
+        type : "POST",
+        url:"../home/checkCode.do",
+        data : {
+            code : code
+        },
+        success:function(data){
+            var result = eval('(' + data + ')');
+            if(result == 'SUC'){
+                    //$("#lgForm").attr("action","../home/doLogin");
+                   //$("#lgForm").submit();
+                $("#lgForm").ajaxSubmit(function (data) {
+                    alert(data);
+                });
+
+               /* $.ajax({
+                    url:"http://139.129.239.172:7710/php/check_usr2.php",
+                    type:"POST",
+                    data:{
+                        "user_id":userName,
+                        "pwd":password
+                    },
+                    dataType:'String',
+                    success:function(data){
+                        alert(data);
+                    }
+                });*/
+
+               // $("#isOk").show();
+                return false;
+            }else{
+                $("#errMsg").html("验证码输入有误！请重新输入.");
+                return false;
+            }
+        }
+    });
+}
+
+function checkCode(){
+    var code = $('#code').val();
+    $("#isOk").hide();
+    $("#errMsg").empty();
+    $("#errMsg").attr("class","msg");
+    if(code.length == 0 || code == '') {
+        $("#errMsg").html("请输入验证码");
+        return false;
+    }
+    $.ajax({
+        type : "POST",
+        url:"../home/checkCode.do",
+        data : {
+            code : code
+        },
+        success:function(data){
+            var result = eval('(' + data + ')');
+            if(result == 'SUC'){
+                $("#isOk").show();
+                return false;
+            }else{
+                $("#errMsg").html("验证码输入有误！请重新输入.");
+                return false;
+            }
+        }
+    });
+
+}
